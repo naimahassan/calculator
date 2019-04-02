460fec4e8afc82e9d412c3044c5e64a3f7af26f0
naimahassan <hassannaima77@gmail.com>
2019-04-02 20:58:06 +0300

    world class calclutor

---
+++ calculator.py
@@ -0,0 +1,32 @@
+def add(a,b):
+	c = a + b
+	print(c)
+
+def subtract(a,b):
+	c = a - b
+	print(c)
+
+def multiply(a,b):
+	c = a * b
+	print(c)
+
+def divide(a,b):
+	c = a / b
+	print(c)
+
+def module(a,b):
+	c = a % b
+	print(c)
+
+def whole_division(a,b):
+	c = a // b
+	print(c)
+
+def percentage(a,b):
+	d = a / b
+	c = d /100
+	print(c)
+
+def expontiate(a,b):
+	c = a ** b 
+	print(c)
